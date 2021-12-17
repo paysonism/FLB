6047332a178b2422c87c0804f28fbaa8cd468f59
Payson Holmes <paysonholmes@yahoo.com>
2021-12-16 17:27:37 -0700

    Delete INSTALL.bat

--- INSTALL.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title FLB Pro - Installing
-ECHO Installing the required packages needed for the bot!
-TIMEOUT 3
-
-py -3 -m pip install -U -r install.bin
-
-ECHO Finished installing required packages, now run START.bat
-PAUSE
